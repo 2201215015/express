68dafca31288a3577614b6961e8499c7c850f14b
2201215015 <2201215015ya@cyber-u.ac.jp>
2025-02-01 04:50:40 +0900

    renew

---
+++ routes/notes.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+const cors = require('cors');
+require('dotenv').config();
+
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri= process.env.MONGODB_URI;
+const client= new MongoClient(uri);
+
+router.get('/', async(req, res) => {
+// データベース、コレクションを指定
+const database= client.db('notes');
+const notes= database.collection('notes');
+
+// idが2のドキュメントを取得
+const query= {id:1};
+const note= await notes.findOne(query);
+
+res.json(note);
+})
+
+module.exports= router;
